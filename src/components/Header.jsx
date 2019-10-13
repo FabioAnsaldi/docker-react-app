e8a7227bfe30f83b1b965925c83f80399c8659ca
MacBook Pro <macbookpro@mbp-di-macbook.uni.ds.port.ac.uk>
2019-10-13 14:10:16 +0100

    First commit: Home & About pages. Default configurations for NextJS

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Header = () => (
+    <div>
+        <Link href="/">
+            <a title="Home Page">Home</a>
+        </Link>
+        <span>&nbsp;</span>
+        <Link href="/about">
+            <a title="About Page">About</a>
+        </Link>
+    </div>
+)
+
+export default Header
